0a8b2ec41c9b6bbbac1429271a25fd5ce2e3cf6a
RobinKrederDeltion <97043715@st.deltion.nl>
2018-05-07 23:37:52 +0200

    alle kat dingen

---
+++ Mario ripoff/Assets/Scripts/Lopen.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lopen : MonoBehaviour
+{
+
+    public Vector3 A;
+    public float hor;
+    public float speed;
+    private SpriteRenderer mijnSpriteRenderer;
+    public Animator anim;
+
+
+
+    private void Awake()
+    {
+
+        mijnSpriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+        void FixedUpdate()
+    {
+
+        hor = Input.GetAxis("Horizontal");
+        A.x = hor;
+        transform.Translate(A * Time.deltaTime * speed);
+        anim.SetFloat("Speed", hor);
+
+        if ( hor < 0)
+        {
+            mijnSpriteRenderer.flipX = true;
+
+
+        }
+        else if (hor > 0)
+        {
+            mijnSpriteRenderer.flipX = false;
+        }
+        }
+
+        
+    }
+
+
