057721ad2c0edd2c2b10bba5c749bc4d690e68c6
RobinKrederDeltion <97043715@st.deltion.nl>
2018-06-27 20:07:39 +0100

    updates

--- Mario ripoff/Assets/Scripts/Lopen.cs
+++ Mario ripoff/Assets/Scripts/Lopen.cs
@@ -10,9 +10,38 @@ public class Lopen : MonoBehaviour
     public float speed;
     private SpriteRenderer mijnSpriteRenderer;
     public Animator anim;
+    public bool grounded;
+    public float jump;
+    private Rigidbody rb;
+    public float jumpForce;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
 
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+            anim.SetTrigger("Land");
+        }
+    }
 
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exited");
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            grounded = false;
+        }
+    }
     private void Awake()
     {
 
@@ -27,6 +56,13 @@ public class Lopen : MonoBehaviour
         transform.Translate(A * Time.deltaTime * speed);
         anim.SetFloat("Speed", hor);
 
+        if(Input.GetButtonDown("Jump")&& grounded == true)
+        {
+            //rb.AddForce(new Vector3(0, jumpForce, 0));
+            rb.velocity += Vector3.up * jumpForce;
+            anim.SetTrigger("Jump");
+        }
+
         if ( hor < 0)
         {
             mijnSpriteRenderer.flipX = true;
@@ -37,9 +73,7 @@ public class Lopen : MonoBehaviour
         {
             mijnSpriteRenderer.flipX = false;
         }
-        }
-
-        
+        } 
     }
 
 
